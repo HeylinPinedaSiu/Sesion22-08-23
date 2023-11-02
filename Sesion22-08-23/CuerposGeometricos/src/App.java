a523c0a8c2de5f6f25fad974837d87711ed66a2e
HeylinPinedaSiu <142961091+HeylinPinedaSiu@users.noreply.github.com>
2023-11-02 17:19:19 -0400

    Update App.java

--- Sesion22-08-23/CuerposGeometricos/src/App.java
+++ Sesion22-08-23/CuerposGeometricos/src/App.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner lector = new Scanner(System.in);
+        
+        //Ejemplo resuelto del prisma
+        
+        //Declaramos las  variables
+        int cantidadLados;
+        double longitudLados, altura, areaLateral,areaTotal,volumen;
+        double perimetroBase, areaBase, apotema
+            
+        //Solicitamos los  datos al usuario
+        System.out.print("Cantidad de lados: ");
+        cantidadLados = lector.nextInt();
+        System.out.print("Altura: ");
+        altura = lector.nextDouble();
+        System.out.print("Longitud de los lados: ");
+        longitudLados = lector.nextDouble
+            
+        //calculamos los  datos
+        perimetroBase = cantidadLados * longitudLados;
+        areaLateral = perimetroBase * altura;
+        double alfa = (Math.PI/180) * (360/cantidadLados);
+        apotema = longitudLados / (2 * Math.tan((alfa)/2));
+        areaBase = (perimetroBase * apotema)/2;
+        areaTotal = areaLateral + 2 * areaBase;
+        volumen = areaBase * altura;
+        
+        //Mostrar los resultados
+        System.out.println("Area lateral: " + areaLateral + " unidades cuadradas.");
+        System.out.println("Area total: " + areaTotal + " unidades cuadradas.");
+        System.out.println("Volumen: " + volumen + " unidades cubicas.");
+        lector.close();
     }
 }
